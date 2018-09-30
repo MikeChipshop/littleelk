3afb83e911517944fec71a918cb00bd5ead61819
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-09-30 13:01:28 +0100

    Initial Commit

---
+++ littleelk/503.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en-GB" prefix="og: http://ogp.me/ns#">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Little Elk | Modern designs and homewares</title>
+        <link href="style.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <meta name="description" content="Modern designs and homewares by Little Elk. Inspired by minimal Scandinavian design."/>
+        <link rel="canonical" href="https://www.littleelk.uk/" />
+        <meta property="og:locale" content="en_GB" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Little Elk | Modern designs and homewares" />
+        <meta property="og:description" content="Modern designs and homewares by Little Elk. Inspired by minimal Scandinavian design." />
+        <meta property="og:url" content="https://www.littleelk.uk/" />
+        <meta property="og:site_name" content="Little Elk" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Modern designs and homewares by Little Elk. Inspired by minimal Scandinavian design." />
+        <meta name="twitter:title" content="Little Elk | Modern designs and homewares" />
+        <meta name="twitter:site" content="@littleelkshop" />
+        <meta name="twitter:creator" content="@littleelkshop" />
+        <script type='application/ld+json'>{"@context":"https:\/\/schema.org","@type":"WebSite","@id":"#website","url":"https:\/\/www.littleelk.uk\/","name":"Little Elk","potentialAction":{"@type":"SearchAction","target":"https:\/\/www.littleelk.uk\/?s={search_term_string}","query-input":"required name=search_term_string"}}</script>
+        <script type='application/ld+json'>{"@context":"https:\/\/schema.org","@type":"Organization","url":"https:\/\/www.littleelk.uk\/","sameAs":["https:\/\/www.facebook.com\/littleelkshop\/","https:\/\/www.instagram.com\/littleelkshop","https:\/\/twitter.com\/littleelkshop"],"@id":"https:\/\/www.littleelk.uk\/#organization","name":"Little Elk","logo":"https:\/\/www.littleelk.uk\/assets\/logos\/yellow-doll.png"}</script>
+    </head>
+    <body>
+        <div class="lesp_splash-wrap">
+            <main class="lesp_splash-cont">
+                <img src="img/little-elk-logo.png" width="200" height="315" alt="Little Elk">
+                <h1>Little Elk</h1>
+                <div class="lesp_splash-desc">
+                    Website coming soon
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
