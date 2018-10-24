205e90c82b1ef2506a76b4da5b51e674af905cdd
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-10-24 18:35:26 +0100

    Various

--- littleelk/header.php
+++ littleelk/header.php
@@ -27,12 +27,15 @@
     <div class="lesp_wrap">
         <div class="lesp_logo">
             <a href="<?php bloginfo('url'); ?>" title="Back to Little Elk home">
+                <div class="lesp_header-village">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/website-village.png">
+                </div>
                 <h1>Little Elk</h1>
             </a>
         </div>
-        <nav class="lesp_global-nav">
+        <!-- <nav class="lesp_global-nav">
             <ul><?php wp_nav_menu( array('theme_location' => 'main_menu' )); ?></ul>
         </nav>
-        <div class="lesp_header-flair"></div>
+-->
     </div>
 </header>
